598bc161057f6f103aeb9044d196c1764e9a3fe7
JayeshPatil18 <jp7470484@gmail.com>
2024-06-23 18:02:06 +0530

    done

--- app/src/main/java/com/apps/netclanui/MainActivity.java
+++ app/src/main/java/com/apps/netclanui/MainActivity.java
@@ -5,20 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
+    LinearLayout refine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.refine);
+        refine = findViewById(R.id.refine);
 
-        imageView.setOnClickListener(new View.OnClickListener() {
+        refine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RefineActivity.class);
